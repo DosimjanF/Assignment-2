48fc169649d1b43bb0dfe412ce306e0a86cd4e2d
Dosimjan <dk123tm@mail.ru>
2024-01-20 09:30:50 +0600

    added comments and provided clarity for student class

--- src/modules/Student.java
+++ src/modules/Student.java
@@ -2,39 +2,39 @@ package modules;
 
 public class Student extends Person {
     private double gpa;
-    public double salary;
+    private double salary;
 
     public Student(){
         super();
-    }
+    }  // Default no argument constructor that creates an instance with only id
 
-    public Student(String name, String surname, double gpa){
+    public Student(String name, String surname, double gpa){  // This constructor lets you create a student object with name, surname and gpa
         super(name, surname);
         setGpa(gpa);
     }
 
     public void setGpa(double gpa) {
         this.gpa = gpa;
-    }
+    }  // This method is a setter for gpa
 
     public double getGpa() {
         return gpa;
-    }
+    }  // This method returns the gpa
 
     @Override
-    public String getPosition() {
+    public String getPosition() {  // This method overrides the getPosition method to return "Student"
         return "Student";
     }
 
     @Override
     public String toString() {
         return super.toString() + " earns " + getPaymentAmount();
-    }
+    }  // Returns the data of object as a string
 
 
     @Override
-    public double getPaymentAmount() {
-        if (gpa > 2.67) {return 36660.0;}
+    public double getPaymentAmount() {  // Returns the payment amount for student according to gpa
+        if (getGpa() > 2.67) {return 36660.0;}
         return 0;
     }
 }
