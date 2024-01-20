2c7acd9c247965d82663ed3574f750f2d0f5c4d8
Dosimjan <dk123tm@mail.ru>
2024-01-20 09:27:05 +0600

    added comments and provided clarity for employee class

--- src/modules/Employee.java
+++ src/modules/Employee.java
@@ -4,9 +4,11 @@ public class Employee extends Person {
     private String position;
     private double salary;
 
-    public Employee() {
+    public Employee() {  // Default no parameter constructor of Employee which only creates it with data
         super();
     }
+
+    // This constructor lets you create an Employee object with name, surname, position and salary
     public Employee(String name, String surname, String position, double salary){
         super(name, surname);
         setPosition(position);
@@ -15,27 +17,24 @@ public class Employee extends Person {
 
     public void setPosition(String position) {
         this.position = position;
-    }
+    }  // This method is a setter for position
 
     public void setSalary(double salary) {
         this.salary = salary;
-    }
+    }  // This method is a setter for salary
 
     public String getPosition() {
         return position;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
+    }  // This method returns the position
 
     @Override
-    public String toString() {
-        return super.toString() + " earns " + getSalary();
-    }
-
-    @Override
-    public double getPaymentAmount() {
+    public double getPaymentAmount() {  // This method returns te value of salary.
         return this.salary;
     }
+    @Override
+    public String toString() {
+        return super.toString() + " earns " + getPaymentAmount();
+    }  // Returns the data of object as a string
+
+
 }
